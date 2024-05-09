ec56ef7dbdf4f15f5212e4b19858d55c8d111533
Roger Santos <rogerioantoniodossantos@outlook.com>
2024-05-09 15:02:40 -0300

    Defining model City properties

--- Models/City.cs
+++ Models/City.cs
@@ -1,9 +1,23 @@
+using System.ComponentModel.DataAnnotations;
 namespace MyAPP.Models;
 
 public class City
 {
+    [Key]
+    [ScaffoldColumn(false)]
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "The {0} field is required")]
+    [StringLength(60, MinimumLength = 3)]
     public string? Name { get; set; }
+
+    [Required(ErrorMessage = "The {0} field is required")]
+    [StringLength(2)]
     public string? State { get; set; }
+    
+    [DataType(DataType.DateTime)]
+    [ScaffoldColumn(false)]
+    public DateTime CreatedAt { get; set;}
+
     public bool Active { get; set; }
 }
