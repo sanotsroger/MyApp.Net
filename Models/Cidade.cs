cef85040bab35952751117f2dc1f807e95736e98
Roger Santos <rogerioantoniodossantos@outlook.com>
2024-05-09 14:17:33 -0300

    Rename Cidade to City

--- Models/Cidade.cs
+++
@@ -1,9 +0,0 @@
-namespace MyAPP.Models;
-
-public class Cidade
-{
-    public int Id { get; set; }
-    public string? Nome { get; set; }
-    public string? UF { get; set; }
-    public bool Ativo { get; set; }
-}
---
+++ Models/City.cs
@@ -0,0 +1,9 @@
+namespace MyAPP.Models;
+
+public class City
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? State { get; set; }
+    public bool Active { get; set; }
+}
